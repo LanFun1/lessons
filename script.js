3203659cbebaca1dacaf75755e6174356b66ee97
Daniel <vik-vay.av@yandex.ru>
2021-06-23 17:42:36 +0300

    Исправил ошибки

--- script.js
+++ script.js
@@ -8,48 +8,41 @@ let isNumber = function(n) {
 let money;    
 let costs = [];
 
-
-appData.asking = function (){
-  appData.addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-  appData.deposit = confirm("Есть ли у вас депозит в банке?");
-  for(let i = 0; i < 2; i++){
-    costs[i] = prompt("Введите обязательную статью расходов?");
-  }
-  while(!isNumber(costs[2]) || !isNumber(costs[3])){
-      costs[2] = prompt("Во сколько это обойдется?");
-      costs[3] = prompt("Во сколько это обойдется?");
-  }
-  let answer = new Object();
-  answer[costs[0]] = +costs[2];
-  answer[costs[1]] = +costs[3];
-  
-  return answer;
-}
-appData.expenses = appData.asking();
 let start = function() {
   do {
     money = prompt("Ваш месячный доход?");
   }
   while(!isNumber(money));
 };
-
 start();
 
+appData.asking = function (){
+  appData.addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+  appData.deposit = confirm("Есть ли у вас депозит в банке?");
+  while(!isNumber(costs[2]) || !isNumber(costs[3])){
+      costs[0] = prompt("Введите обязательную статью расходов?");
+      costs[2] = prompt("Во сколько это обойдется?");
+      costs[1] = prompt("Введите обязательную статью расходов?");
+      costs[3] = prompt("Во сколько это обойдется?");
+  }
+  appData.expenses = new Object();
+  appData.expenses[costs[0]] = +costs[2];
+  appData.expenses[costs[1]] = +costs[3];
+  
+}
+appData.asking();
 
-function getBudget(money, cost, cost1, data){
+appData.getBudget = function getBudget(money, cost, cost1, data){
   if(data === 1)
   {
-    let budgetMonth = money - (cost+cost1);
-    return budgetMonth;
+    appData.budgetMonth= money - (cost+cost1);
   }
   else{
-    let budgetDay = Math.floor((money - (cost+cost1)) / 30);
-    return budgetDay;
+    appData.budgetDay = Math.floor((money - (cost+cost1)) / 30);
   }
 }
-
-appData.budgetMonth = getBudget(money, +costs[2], +costs[3], 1);
-appData.budgetDay = getBudget(money, +costs[2], +costs[3], 2);
+appData.getBudget(money, +costs[2], +costs[3], 1);
+appData.getBudget(money, +costs[2], +costs[3], 2);
 
 
 appData.ExpensesMonth = 0;
@@ -87,13 +80,9 @@ appData.getStatusIncome = function (budgetDay){
   }
 };
 console.log(appData);
-let exp = appData.ExpensesMonth();
-console.log('exp: ', exp);
-let target = appData.getTargetMonth(appData.budgetMonth, mission);
-console.log('target: ', target);
-let status = appData.getStatusIncome(appData.budgetDay);
-console.log('status: ', status);
-/* console.log(appData.getStatusIncome); */
+console.log(appData.ExpensesMonth());
+console.log(appData.getTargetMonth(appData.budgetMonth, mission));
+console.log(appData.getStatusIncome(appData.budgetDay));
 console.log("Наша программа включает в себя данные:\n");
 for(let key in appData){
   console.log("Ключ: "+key+"\tЗначениие: "+appData[key]);
